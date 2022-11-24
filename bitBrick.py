e9f2a8e26648ce6ee03ba9a6250dbba03a650541
BrickGoat <BrickGoat@gmail.com>
2022-11-24 14:15:07 -0500

    extracted name and port from announce url for udp connection

--- bitBrick.py
+++ bitBrick.py
@@ -1,4 +1,5 @@
 import bencodepy
+import urllib.parse
 import hashlib
 import secrets
 import string
@@ -7,9 +8,13 @@ def decodeTorrent(path):
     metaInfo = bencodepy.Bencode().read(path)
     info = metaInfo[b'info']
     announce_url = metaInfo[b'announce'].decode('UTF-8')
-    return info, announce_url
+    begin = str(announce_url).rindex("/") + 1
+    end = str(announce_url).rindex(":")
+    name = announce_url[begin:end]
+    port = announce_url[end+1:len(announce_url)]
+    return info, name, port
 
-def makeRequest(info, announce):
+def makeAnnounceRequest(info, name, port):
     h = hashlib.sha1()
     encodedInfo = bencodepy.encode(info)
     h.update(encodedInfo)
@@ -18,9 +23,15 @@ def makeRequest(info, announce):
     randStr = ''.join(secrets.choice(sequence) for i in range(12))
     peerId = f"-BB0001-{randStr}"
 
+
+def makeUdpConnection(url):
+    pass
+
 def downloadTorrent(path):
-    info, announce = decodeTorrent(path)
-    req = makeRequest(info, announce)
+    info, name, port = decodeTorrent(path)
+    req = makeAnnounceRequest(info, name, port)
+
+downloadTorrent("./big-buck-bunny.torrent")
 
 torrent_path = input("What is path to torrent? ")
 

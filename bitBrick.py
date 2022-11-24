54f738ab1ad6cd069a4e3dab735058efea890ede
BrickGoat <BrickGoat@gmail.com>
2022-11-24 13:20:44 -0500

    20 byte sha1 info hash

--- bitBrick.py
+++ bitBrick.py
@@ -1,4 +1,5 @@
 import bencodepy
+import hashlib
 
 def decodeTorrent(path):
     metaInfo = bencodepy.Bencode().read(path)
@@ -6,8 +7,16 @@ def decodeTorrent(path):
     announce_url = metaInfo[b'announce'].decode('UTF-8')
     return info, announce_url
 
+def makeRequest(info, announce):
+    h = hashlib.sha1()
+    encodedInfo = bencodepy.encode(info)
+    h.update(encodedInfo)
+    infoHash = h.digest()
+    
+
 def downloadTorrent(path):
     info, announce = decodeTorrent(path)
+    req = makeRequest(info, announce)
 
 torrent_path = input("What is path to torrent? ")
 

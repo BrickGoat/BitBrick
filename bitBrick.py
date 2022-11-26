57773fd69e917c2b5904060a066b4eb03e810e9d
BrickGoat <BrickGoat@gmail.com>
2022-11-26 18:54:29 -0500

    added tracker connection request

--- bitBrick.py
+++ bitBrick.py
@@ -3,36 +3,73 @@ import urllib.parse
 import hashlib
 import secrets
 import string
+import time
+import math
+import socket, struct
 
 def decodeTorrent(path):
-    metaInfo = bencodepy.Bencode().read(path)
+    metaInfo = bencodepy.Bencode(dict_ordered=True).read(path)
     info = metaInfo[b'info']
     announce_url = metaInfo[b'announce'].decode('UTF-8')
-    begin = str(announce_url).rindex("/") + 1
-    end = str(announce_url).rindex(":")
-    name = announce_url[begin:end]
-    port = announce_url[end+1:len(announce_url)]
-    return info, name, port
+    
+    announce_urls = []
+    for announce_url in metaInfo[b'announce-list']:
+        url = announce_url[0].decode()
+        if url[0:3] == "udp":
+            begin = str(url).rindex("/") + 1
+            end = str(url).rindex(":")
+            name = url[begin:end]
+            port = url[end+1:len(url)]
+            announce_urls.append((name, int(port)))
+    return info, announce_urls
 
-def makeAnnounceRequest(info, name, port):
+def makeAnnounceRequest(info, announce_urls):
     h = hashlib.sha1()
     encodedInfo = bencodepy.encode(info)
     h.update(encodedInfo)
     infoHash = h.digest()
+    infoHash = bytes(urllib.parse.quote(infoHash), 'utf-8')
     sequence = string.ascii_letters + string.digits
     randStr = ''.join(secrets.choice(sequence) for i in range(12))
-    peerId = f"-BB0001-{randStr}"
+    peerId = bytes(f"-BB0001-{randStr}", 'utf-8')
+    
+    for i in range(len(announce_urls)):
+        name = announce_urls[i][0] 
+        port = announce_urls[i][1]
+        connection_packet, trans_id = getConnectPack()
+        resp = makeUdpConnection(name, port, connection_packet)
 
 
-def makeUdpConnection(url):
-    pass
+def getConnectPack():
+    protocol_id = 0x41727101980
+    action = 0
+    sequence = string.digits
+    trans_id = ''.join(secrets.choice(sequence) for i in range(4))
+    return struct.pack(">QII", protocol_id, action, int(trans_id)), trans_id
+
+
+def makeUdpConnection(name, port, packet):
+    buffSize = 4096
+    UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    n = 0
+    while n < 3:
+        try:
+            timeout = 15 * math.pow(2, n)
+            print(timeout)
+            UDPClientSocket.settimeout(timeout)
+            n = n + 1
+            print((name, port))
+            UDPClientSocket.sendto(packet, (name, port))
+            resp = UDPClientSocket.recvfrom(buffSize)
+            return resp[0]
+        except Exception as e:
+            print(e)
+    return None
+    
 
 def downloadTorrent(path):
-    info, name, port = decodeTorrent(path)
-    req = makeAnnounceRequest(info, name, port)
+    info, announce_urls = decodeTorrent(path)
+    #print(announce_urls)
+    req = makeAnnounceRequest(info, announce_urls)
 
 downloadTorrent("./big-buck-bunny.torrent")
-
-torrent_path = input("What is path to torrent? ")
-
-downloadTorrent(torrent_path)

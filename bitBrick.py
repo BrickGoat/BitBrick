1c34561acf3f79360f395a1fa83fb84d2af4c26d
BrickGoat <BrickGoat@gmail.com>
2022-12-01 02:42:15 -0500

    attempt at fixing tracker connection

--- bitBrick.py
+++ bitBrick.py
@@ -3,17 +3,27 @@ import urllib.parse
 import hashlib
 import secrets
 import string
-import time
 import math
 import socket, struct
 
+# Main function
+def downloadTorrent(path):
+    info, announce_urls, length = decodeTorrent(path)
+    req = makeAnnounceRequest(info, [('tracker.opentrackr.org', 1337)], length)
+
+# Decode bencoded torrent file and return info key, list of udp trackers, and total size of files
 def decodeTorrent(path):
-    metaInfo = bencodepy.Bencode(dict_ordered=True).read(path)
+    metaInfo = bencodepy.Bencode(dict_ordered=True, dict_ordered_sort=True).read(path)
     info = metaInfo[b'info']
-    announce_url = metaInfo[b'announce'].decode('UTF-8')
-    
+    announce_list = metaInfo[b'announce-list']
+    length = 0
+    if "length" in info.keys(): # single file torrent
+        length = info[b'length']
+    else:
+        for fileDict in info[b'files']: # Multiple files
+            length += fileDict[b'length']
     announce_urls = []
-    for announce_url in metaInfo[b'announce-list']:
+    for announce_url in announce_list:
         url = announce_url[0].decode()
         if url[0:3] == "udp":
             begin = str(url).rindex("/") + 1
@@ -21,43 +31,49 @@ def decodeTorrent(path):
             name = url[begin:end]
             port = url[end+1:len(url)]
             announce_urls.append((name, int(port)))
-    return info, announce_urls
+    return info, announce_urls, length
 
-def makeAnnounceRequest(info, announce_urls):
+"""
+    Orchestrates calls to other function in order to request connection id from tracker 
+    then make announcement in order to get list of peers
+     - Currently the announcement only returns my own information? 
+"""
+def makeAnnounceRequest(info, announce_urls, length):
+    # Bencode the info key then create a sha1 hash of it before url encoding it and converting it back to bytes
     h = hashlib.sha1()
     encodedInfo = bencodepy.encode(info)
     h.update(encodedInfo)
-    infoHash = h.digest()
-    infoHash = bytes(urllib.parse.quote(infoHash), 'utf-8')
+    infoHash = h.hexdigest()
+    infoHash = urllib.parse.quote_plus(infoHash).encode('UTF-8')
+    # Create the peerId
     sequence = string.ascii_letters + string.digits
     randStr = ''.join(secrets.choice(sequence) for i in range(12))
     peerId = bytes(f"-BB0001-{randStr}", 'utf-8')
-    
+    # Attempt to make a connection with each tracker
     for i in range(len(announce_urls)):
         name = announce_urls[i][0] 
         port = announce_urls[i][1]
+        # Create conncection packet and retrieve connection id
         connection_packet, trans_id = getConnectPack()
-        resp = makeUdpConnection(name, port, connection_packet)
-        print(resp)
+        resp = sendUdpPacket(name, port, connection_packet)
         if resp is None or readConnectionResp(resp, trans_id) is None:
-            #announce_urls.pop(i)
             continue
         connection_id = readConnectionResp(resp, trans_id)
-        announcement_pack, trans_id = getAnnouncePack(connection_id, infoHash, peerId)
+        # Create announcement packet and retrieve peer list
+        announcement_pack = getAnnouncePack(connection_id, infoHash, peerId, trans_id=trans_id, left=length)
         url = announce_urls.pop(i)
         announce_urls.insert(0, url)
         for i in range(len(announce_urls)):
             name = announce_urls[i][0] 
             port = announce_urls[i][1]
-            resp = makeUdpConnection(name, port, announcement_pack)
+            resp = sendUdpPacket(name, port, announcement_pack)
             if resp is None or readAnnounceResp(resp, trans_id) is None:
                 continue
             peers = readAnnounceResp(resp, trans_id)
             print(peers)
             raise Exception("Placeholder")
 
-
-
+# Creates udp tracker connection packet
 def getConnectPack():
     protocol_id = 0x41727101980
     action = 0
@@ -65,27 +81,31 @@ def getConnectPack():
     trans_id = ''.join(secrets.choice(sequence) for i in range(4))
     return struct.pack(">QII", protocol_id, action, int(trans_id)), trans_id
 
-def getAnnouncePack(connection_id, info_hash, peer_id,left =0, uploaded=0,  downloaded=0, event=3):
+# Creates udp tracker announcement packet
+def getAnnouncePack(connection_id, info_hash, peer_id, trans_id=None, left =0, uploaded=0,  downloaded=0, event=2):
     sequence = string.digits
     action = 1
-    trans_id = ''.join(secrets.choice(sequence) for i in range(4))
-    print(info_hash)
-    return struct.pack(">QII20s20sQQQIIIiH", connection_id, action, int(trans_id), info_hash, peer_id, downloaded, left, uploaded, event, 0, 0, -1, 6881), trans_id
+    if trans_id is None:
+        trans_id = ''.join(secrets.choice(sequence) for i in range(4))
+    key = ''.join(secrets.choice(sequence) for i in range(4))
+    return struct.pack(">QII20s20sQQQIIIiH", connection_id, action, int(trans_id), info_hash, peer_id, downloaded, left, uploaded, event, 0, int(key), -1, 6881)
 
+# Returns connection id if tracker response is valid else None
 def readConnectionResp(resp, id):
     action, trans_id, connect_id = struct.unpack(">IIQ", resp)
-    print(action)
     if action == 0 and int(trans_id) == int(id):
         return connect_id
     return None
 
+# Returns peerlist from announcement response
 def readAnnounceResp(resp, id):
     peerList = []
     action, trans_id = struct.unpack(">II", resp[:8])
     if action != 1 or int(trans_id) != int(id):
-        if action == 3:
+        if action == 3: # if error, print error msg
             print(resp[8:])
         return None
+    # valid response, read variable amount of peers from rest of response packet
     peers = resp[20:]
     peerLen = 6
     peerCount = int(len(peers)/6)
@@ -98,8 +118,8 @@ def readAnnounceResp(resp, id):
         peerList.append(peerDict)
     return peerList
     
-
-def makeUdpConnection(name, port, packet):
+# send udp packet to (name, port) destination, retry connection based on bittorent specification
+def sendUdpPacket(name, port, packet):
     buffSize = 4096
     UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
     n = 0
@@ -112,15 +132,11 @@ def makeUdpConnection(name, port, packet):
             print((name, port))
             UDPClientSocket.sendto(packet, (name, port))
             resp = UDPClientSocket.recvfrom(buffSize)
+            UDPClientSocket.close()
             return resp[0]
         except Exception as e:
             print(e)
     return None
     
 
-def downloadTorrent(path):
-    info, announce_urls = decodeTorrent(path)
-    #print(announce_urls)
-    req = makeAnnounceRequest(info, announce_urls)
-
-downloadTorrent("./big-buck-bunny.torrent")
+downloadTorrent("./cosmos-laundromat.torrent")
